3d567d216f63e7c423f0397a62d27dfdab613059
NepheilS <batuhanfidan07@hotmail.com>
2024-06-06 15:20:20 +0300

    login and success

---
+++ odev/src/Success.jsx
@@ -0,0 +1,10 @@
+function Success() {
+  return (
+    <div>
+      <h1>Hoşgeldiniz!</h1>
+      Hayırlı olsun, başardın!
+    </div>
+  );
+}
+
+export default Success;
--- odev/src/main.jsx
+++ odev/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
