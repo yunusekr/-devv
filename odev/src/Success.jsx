daad61b0f0604b1d16eff81038cc88f776b00e9d
NepheilS <batuhanfidan07@hotmail.com>
2024-06-06 15:27:28 +0300

    validation update

--- odev/src/Success.jsx
+++ odev/src/Success.jsx
@@ -2,7 +2,14 @@ function Success() {
   return (
     <div>
       <h1>Hoşgeldiniz!</h1>
-      Hayırlı olsun, başardın!
+      <h2>Welcome!</h2>
+      <h3>Bonjuor!</h3>
+      <h4>Ola!</h4>
+      <h5>Привет!</h5>
+      <h6>こんにちは!</h6>
+      Hayırlı olsun, başardın! , Congrats, you did it!, Félicitations, vous
+      l'avez fait!, ¡Felicitaciones, lo lograste!, Поздравляем, вы сделали это!,
+      おめでとうございます、やったね！
     </div>
   );
 }
